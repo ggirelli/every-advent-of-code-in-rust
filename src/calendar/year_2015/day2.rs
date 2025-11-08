b3819e63538f1750a1b4f9be096ab5e4dd434326
Gabriele Girelli <1636105+ggirelli@users.noreply.github.com>
2025-11-08 16:36:54 +0100

    feat: 2015-2-2

--- src/calendar/year_2015/day2.rs
+++ src/calendar/year_2015/day2.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 use crate::io::read_lines;
 
 /// Calculate the surface of a single box.
-fn get_box_surface(line: &String) -> Result<i32, Error> {
+fn get_box_surface(line: &str) -> Result<i32, Error> {
     let mut total_box_surface: i32 = 0;
 
     let sides: Vec<i32> = line
@@ -37,6 +37,35 @@ pub fn run_part_1(input: PathBuf) -> Result<(), Error> {
     Ok(())
 }
 
+/// Calculate the smalles box face perimeter.
+fn get_smallest_box_face_perimeter(line: &str) -> Result<i32, Error> {
+    let mut sides_vec: Vec<i32> = line
+        .split(&"x")
+        .map(|d| d.parse::<i32>().unwrap())
+        .collect();
+    assert_eq!(sides_vec.len(), 3);
+    sides_vec.sort();
+
+    let sides: [i32; 3] = [
+        *sides_vec.first().unwrap(),
+        *sides_vec.get(1).unwrap(),
+        *sides_vec.get(2).unwrap(),
+    ];
+    let wrap_length: i32 = 2 * sides[0] + 2 * sides[1];
+    let ribbon_length: i32 = sides.iter().product::<i32>();
+
+    Ok(wrap_length + ribbon_length)
+}
+
+pub fn run_part_2(input: PathBuf) -> Result<(), Error> {
+    let mut total_length: i32 = 0;
+    for line in read_lines(input)? {
+        total_length += get_smallest_box_face_perimeter(&line)?;
+    }
+    tracing::info!("Total length: {}", total_length);
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,8 +73,14 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn test_total_box_area() {
-        assert_eq!(get_box_surface(&"2x3x4".to_string()).unwrap(), 58);
-        assert_eq!(get_box_surface(&"1x1x10".to_string()).unwrap(), 43);
+    fn test_get_box_surface() {
+        assert_eq!(get_box_surface(&"2x3x4").unwrap(), 58);
+        assert_eq!(get_box_surface(&"1x1x10").unwrap(), 43);
+    }
+
+    #[test]
+    fn test_ribbon_feet_for_box() {
+        assert_eq!(get_smallest_box_face_perimeter(&"2x3x4").unwrap(), 34);
+        assert_eq!(get_smallest_box_face_perimeter(&"1x1x10").unwrap(), 14);
     }
 }
